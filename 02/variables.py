18a06829b0a66ea20d07e41e51f155f059403879
verebelyi15 <verebelyilaura15@gmail.com>
2021-09-17 15:25:16 +0200

    2. óra

---
+++ 02/variables.py
@@ -0,0 +1,15 @@
+kiscica = 5
+print (kiscica + 5)
+print("kiscica")
+
+
+width, length  = 4, 5 # m
+area = width * length
+print (" A szoba területe: ", area, "m^2.")
+length2 = 2 # m
+area = width * length2
+print ("A szoba területe:", area, "m^2.")
+
+width3 = length3 = 5
+print (" A szoba területe:", width3 * length3, "m^2.")
+
---
+++ minutes_in_a_week/minutes_in_a_week.py
@@ -0,0 +1,11 @@
+
+hour = 60
+day = 24 * hour
+week = 7 * day
+
+print ("a héten", hour * day * week, "perc lenne")
+
+days_in_a_week = 7
+hours_in_a_day = 24
+minutes_in_an_hour = 60
+minutes_in_a_week = minutes_in_an_hour * hours_in_a_day
